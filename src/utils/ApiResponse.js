93658e32c31bfc00befeee9dafb7bde2a28da2a0
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2025-11-19 00:45:34 +0500

    added utils added and middlewares initiated

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+  constructor(statusCode, data, message = "SUCCESS") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.success = statusCode < 400;
+    this.message = message;
+  }
+}
+export { ApiResponse };
